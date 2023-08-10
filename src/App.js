8a9c909453067675d3f387527ab35ad925f25193
Ruth <ruthcassidy172@gmail.com>
2023-08-10 15:15:28 +0100

    Add state in order to count button clicks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      Show Animals
+      <button onClick={handleClick}>Add Animal</button>
+      <p>Animals to show: {count}</p>
     </div>
   );
 }
