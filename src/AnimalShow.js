8b13739b802057562d83f61533a4a7282abb3334
Ruth <ruthcassidy1221@gmail.com>
2023-08-11 12:24:56 +0100

    Add the image imports and displayed in jsx

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,6 +1,25 @@
+import bird from './images/bird.svg';
+import cat from './images/cat.svg';
+import cow from './images/cow.svg';
+import dog from './images/dog.svg';
+import gator from './images/gator.svg';
+import heart from './images/heart.svg';
+import horse from './images/horse.svg';
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+}
+
 function AnimalShow({ type }) {
     return (
-        <div>{ type }</div>
+        <div>
+            <img src={svgMap[type]} />
+        </div>
     )
 }
 
